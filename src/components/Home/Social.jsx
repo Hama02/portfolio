ac4f493b570725b4eeefeb27a6758f1dd4628484
Mohamed <mohamedbenjdira5@gmail.com>
2023-02-11 14:01:40 +0100

    Initial Commit

---
+++ src/components/Home/Social.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.instagram.com/benjdira.mohamed/"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="uil uil-instagram"></i>
+      </a>
+
+      <a
+        href="https://twitter.com/BnjHama"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i class="uil uil-twitter"></i>
+      </a>
+
+      <a
+        href="https://github.com/Hama02"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <uil className="uil-github-alt"></uil>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
